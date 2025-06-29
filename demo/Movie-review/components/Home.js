6f65f2f58dc7d17418e088d2cdba4910dae0a38a
Dinesh2565 <116051648+Dinesh2565@users.noreply.github.com>
2025-06-29 10:52:44 +0530

    Delete demo directory

--- demo/Movie-review/components/Home.js
+++
@@ -1,9 +0,0 @@
-import Hero from "./Hero"
-
-const Home = ( {movies}) => {
-  return (
-    <Hero movies={movies} />
-  )
-}
-
-export default Home
--- demo/Movie-review/components/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NotFound = () => {
-  return (
-    <div>This page cannot be found</div>
-  )
-}
-
-export default NotFound
